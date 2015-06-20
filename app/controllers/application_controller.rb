f74810ad5c347d0402caa1313d0b57dd1d704d74
Carlos Vilhena <carlosvilhena@gmail.com>
2015-06-20 15:23:00 +0100

    make sure auth works across all endpoints

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,9 +6,8 @@ class ApplicationController < ActionController::Base
   private
 
   def authenticate_user_from_token!
-    authenticate_with_http_token do |token, options|
-      user_email = options[:email].presence
-      user = user_email && User.find_by_email(user_email)
+    authenticate_or_request_with_http_token do |token, options|
+      user = User.find_by(authentication_token: token)
 
       if user && Devise.secure_compare(user.authentication_token, token)
         sign_in user, store: false
