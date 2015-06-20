111408267fbf29fbc53da772d46414b9bc023355
Carlos Vilhena <carlosvilhena@gmail.com>
2015-06-20 18:45:53 +0100

    fixing mistake - son is not navigational
    and shouldn’t handle flash messages for example.

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -1,5 +1,4 @@
 # Use this hook to configure devise mailer, warden hooks and so forth.
-# Many of these configuration options can be set straight in your model.
 Devise.setup do |config|
   # The secret key used by Devise. Devise uses this key to generate
   # random tokens. Changing this key will render invalid all existing
@@ -230,7 +229,7 @@ Devise.setup do |config|
   # should add them to the navigational formats lists.
   #
   # The "*/*" below is required to match Internet Explorer requests.
-  config.navigational_formats = ['*/*', :json]
+  config.navigational_formats = ['*/*']
 
   # The default HTTP method used to sign out a resource. Default is :delete.
   config.sign_out_via = :delete
